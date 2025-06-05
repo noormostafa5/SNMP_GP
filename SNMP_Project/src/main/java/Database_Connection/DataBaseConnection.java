0f7aa17301f052ad8339056bf9cb3f0061ef0837
Mayar Hesham Fahmy <152290636+MayarHeshamFahmy@users.noreply.github.com>
2025-06-06 01:04:23 +0300

    -

--- SNMP_Project/src/main/java/Database_Connection/DataBaseConnection.java
+++ SNMP_Project/src/main/java/Database_Connection/DataBaseConnection.java
@@ -3,9 +3,8 @@ package Database_Connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-//my-snmp-public.ca5cwqo86nt5.us-east-1.rds.amazonaws.com
 public class DataBaseConnection {
-    private static final String URL = "jdbc:postgresql://my-snmp-public.ca5cwqo86nt5.us-east-1.rds.amazonaws.com:5432/postgres";
+    private static final String URL = "jdbc:postgresql://your-end-point:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASS = "my-aws-database-password";
 
