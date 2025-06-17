7919ec65d97c4b9e9ae0e947a2359a1d9f8484c2
noormostafa5 <93129634+noormostafa5@users.noreply.github.com>
2025-06-17 20:13:32 +0300

    Update DataBaseConnection.java

--- SNMP_Project/src/main/java/Database_Connection/DataBaseConnection.java
+++ SNMP_Project/src/main/java/Database_Connection/DataBaseConnection.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DataBaseConnection {
-    private static final String URL = "jdbc:postgresql://my-snmp-public.ca5cwqo86nt5.us-east-1.rds.amazonaws.com:5432/snmp";
+    private static final String URL = "jdbc:postgresql://host_name";
     private static final String USER = "postgres";
-    private static final String PASS = "Mayar123m";
+    private static final String PASS = "null";
 
     public static Connection getConnection() throws SQLException {
         System.out.println("success");
